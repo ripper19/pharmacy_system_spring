f6ed029c766ff8080a998818e473cc324f51898e
masternone <masternone@kali.masternone>
2025-12-23 15:40:37 +0300

    logging

--- src/main/java/com/pharmacy/pharmacy_management/service/MedicineService.java
+++ src/main/java/com/pharmacy/pharmacy_management/service/MedicineService.java
@@ -9,9 +9,12 @@ import com.pharmacy.pharmacy_management.repository.MedicineTypeRepository;
 import com.pharmacy.pharmacy_management.utilities.TransactionRetryUtility;
 import jakarta.transaction.Transactional;
 import jakarta.validation.ValidationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 import java.math.BigDecimal;
 import java.util.Optional;
@@ -26,12 +29,12 @@ public class MedicineService {
     private MedicineTypeRepository medicineTypeRepository;
     @Autowired
     private TransactionRetryUtility transactionRetryUtility;
-
-    public Optional<Medicine> getMedicineBy(String medicineName){
+    private final Logger logger = LoggerFactory.getLogger(MedicineService.class);
+    public Optional<Medicine> getMedicineBy(String medicineName) {
         return medicineRepo.findByMedicineName(medicineName);
     }
 
-    public Medicine mapToEntity(MedicineAddDto dto){
+    public Medicine mapToEntity(MedicineAddDto dto) {
         Medicine medicine = new Medicine();
         medicine.setMedicineName(dto.getMedicineName());
         medicine.setSku(dto.getSku());
@@ -46,24 +49,27 @@ public class MedicineService {
     @PreAuthorize("hasRole('ADMIN') or hasRole('SUPERADMIN')")
     public Medicine addMedStock(MedicineAddDto addDto) {
         MedicineAddDto cleanedDto = medicineSanitiseService.sanitize(addDto);
+        return transactionRetryUtility.executeWithRetry(() -> {
+            try {
+                MedicineType type = medicineTypeRepository.findByIgnoreCaseName(cleanedDto.getMedicineType())
+                        .orElseThrow(() -> new ValidationException("Cant find this type in Medicine types. Add the type"));
 
-        return transactionRetryUtility.executeWithRetry(()->{
-        MedicineType type = medicineTypeRepository.findByIgnoreCaseName(cleanedDto.getMedicineType())
-                .orElseThrow(()-> new ValidationException("Cant find this type in Medicine types. Add the type"));
-        System.out.println("Medtype"+ type.getName());
-
-        if (cleanedDto.getDescription() == null){
-            cleanedDto.setDescription("Description hasn't been provided. Please input description and instructions for various uses");
-        }
-        if (cleanedDto.getLowStockThreshold() == null || cleanedDto.getLowStockThreshold() == 0){
-            cleanedDto.setLowStockThreshold(50);
-        }
-        System.out.println("Medicine type being saved is " + cleanedDto.getMedicineType());
-        Medicine newMed = mapToEntity(cleanedDto);
-        newMed.setCost(BigDecimal.valueOf(0.00));
-        newMed.setMed_type(type);
-        return medicineRepo.save(newMed);
-    });
+                if (cleanedDto.getDescription() == null) {
+                    cleanedDto.setDescription("Description hasn't been provided. Please input description and instructions for various uses");
+                }
+                if (cleanedDto.getLowStockThreshold() == null || cleanedDto.getLowStockThreshold() == 0) {
+                    cleanedDto.setLowStockThreshold(50);
+                }
+                Medicine newMed = mapToEntity(cleanedDto);
+                newMed.setCost(BigDecimal.valueOf(0.00));
+                newMed.setMed_type(type);
+                logger.info("Saved new Medicine{}of type{}with quantity{}", newMed.getMedicineName(), newMed.getMed_type(), newMed.getQuantity());
+                return medicineRepo.save(newMed);
+            }catch (Exception e) {
+                logger.error("Failed to add{}", addDto.getMedicineName());
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     //using add Dto since update uses same fields
@@ -88,11 +94,18 @@ public class MedicineService {
 
     @Transactional
     @PreAuthorize("hasRole('ADMIN') or hasRole('SUPERADMIN')")
-    public String delMedStock(MedicineDeleteDto deleteDto){
-        if (deleteDto.getName() == null) throw new RuntimeException("Empty request");
+    public String delMedStock(MedicineDeleteDto deleteDto) {
+        return transactionRetryUtility.executeWithRetry(() -> {
+            if (deleteDto.getName() == null) throw new RuntimeException("Empty request");
 
-        medicineRepo.findByMedicineName(deleteDto.getName())
-                .orElseThrow(()-> new RuntimeException("This medicine cant be found in database"));
-        return "Medicine deleted" + deleteDto.getName();
+            String cleanedName = HtmlUtils.htmlEscape(deleteDto.getName());
+
+            Medicine toDelete = medicineRepo.findByMedicineName(cleanedName)
+                    .orElseThrow(() -> new RuntimeException("This medicine cant be found in database"));
+            medicineRepo.delete(toDelete);
+            return "Medicine deleted" + cleanedName;
+        });
     }
 }
+
+
